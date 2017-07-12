244bd79e522e69ce12b70f25f25fd62bf0347686
Sara Jade <wisconsinsara@gmail.com>
2017-07-12 14:56:33 -0700

    Update view

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,40 +1,42 @@
 /* jshint browser: true, devel: true, esversion: 6 */
 
-//$(document).ready(function(websiteData) {
-  'use strict';
-  //Test that base.css, jQuery CDN & portfolio.js are operational
-  //$('h1').addClass('blue');
+'use strict';
+//Test that base.css, jQuery CDN & portfolio.js are operational
+//$('h1').addClass('blue');
 
-  let websites = [];
+let websites = [];
 
-  // constructor
-  function Website (websiteData) {
-    this.title = websiteData.title;
-    this.url = websiteData.url;
-    this.desc = websiteData.desc;
-    this.author = websiteData.author;
-  }
+// constructor
+function Website (websiteData) {
+  this.title = websiteData.title;
+  this.url = websiteData.url;
+  this.desc = websiteData.desc;
+  this.author = websiteData.author;
+  this.publishedOn = websiteData.publishedOn;
+}
 
-  // Build website to use on index.html
-  Website.prototype.toHtml = function() {
-    let $newWebsite = $('article.template').clone();
-    $newWebsite.removeClass('template');
+// Build a toHTML() to use on Website to use on index.html
+// We could put this in the constructor, but then each object has to have the method,
+// and methods are heavyweight.
+// Note: A capitalized object is a constructor!
+Website.prototype.toHtml = function() {
+  let $newWebsite = $('article.template').clone();
+  $newWebsite.removeClass('template');
 
-    $newWebsite.find('h1').text(this.title);
-    $newWebsite.find('.url').text(this.author).attr('href', this.url);
-    $newWebsite.find('.desc').html(this.desc);
+  $newWebsite.find('h1').text(this.title);
+  $newWebsite.find('.url').text(this.author).attr('href', this.url);
+  $newWebsite.find('.desc').html(this.desc);
+  $newWebsite.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newWebsite.append('<hr>');
+  return $newWebsite;
+};
 
-    $newWebsite.append('<hr>');
-    return $newWebsite;
-  };
+// Push website into websites array
+websiteData.forEach(function(websiteObject) {
+  websites.push(new Website(websiteObject));
+});
 
-  // Push website into websites array
-  websiteData.forEach(function(websiteObject) {
-    websites.push(new Website(websiteObject));
-  });
-
-  // Append website data to index.html
-  websites.forEach(function(website) {
-    $('#websites').append(website.toHtml());
-  });
-//});
+// Append website data to index.html
+websites.forEach(function(website) {
+  $('#websites').append(website.toHtml());
+});
--- scripts/websiteTable.js
+++ scripts/websiteTable.js
@@ -7,24 +7,28 @@ let websiteData = [
     title: 'MetaSara',
     url:   'http://metasara.herokuapp.com/',
     author: 'Sara Jade',
-    desc:  '<p>My site of sites</p>'
+    desc:  '<p>My site of sites</p>',
+    publishedOn: '2017-4-01'
   },
   {
     title: 'StepTwoSara',
     url:   'https://dividebysara.github.io/',
     author: 'Sara Jade',
-    desc:  '<p>APIs and a scavenger hunt can be found on this site I built with Codecademy'
+    desc:  '<p>APIs and a scavenger hunt can be found on this site I built with Codecademy',
+    publishedOn: '2017-1-01'
   },
   {
     title: 'DivideBySara',
     url:   'http://sarajade.co.nf/',
     author: 'Sara Jade',
-    desc:  '<p>This fun-filled site features games and a Data Tables project</p>'
+    desc:  '<p>This fun-filled site features games and a Data Tables project</p>',
+    publishedOn: '2017-3-15'
   },
   {
     title: 'The Bright Side',
     url:   'http://itpharmacist.dx.am/portfolio/',
     author: 'Sara Jade',
-    desc:  '<p>This is the site that started it all - my very first website!</p>'
+    desc:  '<p>This is the site that started it all - my very first website!</p>',
+    publishedOn: '2015-12-18'
   }
 ];
