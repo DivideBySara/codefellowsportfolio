375d880de2b9b0f576df4d25d6b25038c67e6a4a
Sara Jade <wisconsinsara@gmail.com>
2017-07-13 15:06:14 -0700

    Update .css's to improve SMACCS

--- scripts/websiteTable.js
+++ scripts/websiteTable.js
@@ -7,28 +7,28 @@ let websiteData = [
     title: 'MetaSara',
     url:   'http://metasara.herokuapp.com/',
     author: 'Sara Jade',
-    desc:  '<p>My site of sites</p>',
+    desc:  '<h4>My site of sites</h4>',
     publishedOn: '2017-4-01'
   },
   {
     title: 'StepTwoSara',
     url:   'https://dividebysara.github.io/',
     author: 'Sara Jade',
-    desc:  '<p>APIs and a scavenger hunt can be found on this site I built with Codecademy',
+    desc:  '<h4>APIs and a scavenger hunt can be found on this site I built with Codecademy</h4>',
     publishedOn: '2017-1-01'
   },
   {
     title: 'DivideBySara',
     url:   'http://sarajade.co.nf/',
     author: 'Sara Jade',
-    desc:  '<p>This fun-filled site features games and a Data Tables project</p>',
+    desc:  '<h4>This fun-filled site features games and a Data Tables project</h4>',
     publishedOn: '2017-3-15'
   },
   {
     title: 'The Bright Side',
     url:   'http://itpharmacist.dx.am/portfolio/',
     author: 'Sara Jade',
-    desc:  '<p>This is the site that started it all - my very first website!</p>',
+    desc:  '<h4>This is the site that started it all - my very first website!</h4>',
     publishedOn: '2015-12-18'
   }
 ];
