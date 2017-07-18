dd74439f3527fe6c76aeda766e4fb46cc584ae42
DivideBySara <DivideBySara@users.noreply.github.com>
2017-07-18 16:06:36 -0700

    Update portfolio.js to remove commented out code
    There was commented out code on this file per Tracey

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -13,22 +13,6 @@ function Website (websiteData) {
   this.publishedOn = websiteData.publishedOn;
 }
 
-// Build a toHTML() to use on Website to use on index.html
-// We could put this in the constructor, but then each object has to have the method,
-// and methods are heavyweight.
-// Note: A capitalized object is a constructor!
-// Website.prototype.toHtml = function() {
-//   let $newWebsite = $('article.template').clone();
-//   $newWebsite.removeClass('template');
-//
-//   $newWebsite.find('h1').text(this.title);
-//   $newWebsite.find('.url').text(this.author).attr('href', this.url);
-//   $newWebsite.find('.desc').html(this.desc);
-//   $newWebsite.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-//   $newWebsite.append('<hr>');
-//   return $newWebsite;
-// };
-
 Website.prototype.toHtml = function() {
   // Handlebars renders articles to the DOM
   let websiteTemplateString = $('#websiteTemplate').html();
