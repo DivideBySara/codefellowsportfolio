aa89a8ff397843a7b94fe21f85073bddedbe4806
Sara Jade <wisconsinsara@gmail.com>
2017-07-18 21:56:45 -0700

    Write a route for the index page

--- server.js
+++ server.js
@@ -12,3 +12,8 @@ const PORT = process.env.PORT || 3000; // The default port is 3000.
 // Static resources (now in public directory) should be passed to app.use()
 const app = express();
 app.use(express.static('./public')); // Sets public as the root directory
+
+// Write a route for the index page
+app.get('.', function(request, response) {
+  response.sendFile('index.html', {root: './public'});
+});
