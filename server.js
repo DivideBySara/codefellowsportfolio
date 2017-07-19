0ed34f2bfaa27222f0b3e5e2bc1cff92bdf55010
Sara Jade <wisconsinsara@gmail.com>
2017-07-18 21:45:19 -0700

    npm init Node.js project

---
+++ server.js
@@ -0,0 +1,10 @@
+/* jshint browser: true, devel: true, esversion: 6 */
+
+'use strict';
+
+// Require express and body-parser
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Set a PORT
+const PORT = process.env.PORT || 3000;
