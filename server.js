d434718325a14d9de155e32e40f834b2ab9eb0c0
Sara Jade <wisconsinsara@gmail.com>
2017-07-18 21:53:49 -0700

    Pass static resources to app.use() and set public as root directory

--- server.js
+++ server.js
@@ -7,4 +7,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 // Set a PORT
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // The default port is 3000.
+
+// Static resources (now in public directory) should be passed to app.use()
+const app = express();
+app.use(express.static('./public')); // Sets public as the root directory
