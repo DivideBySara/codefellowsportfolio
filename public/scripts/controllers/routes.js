2fe1f1433c4878d7d3f2d29842dde6df9588b26f
Sara Jade <wisconsinsara@gmail.com>
2017-07-25 10:53:32 -0700

    Update index.html and routes.js for home and about routes

--- public/scripts/controllers/routes.js
+++ public/scripts/controllers/routes.js
@@ -1,3 +1,10 @@
 'use strict';
 
 var application = application || {};
+
+// route for index.html
+page('/', application.websiteController.initWebsiteController());
+// route for about.html
+page('/about', application.aboutController.initAboutController());
+// call page()
+page();
