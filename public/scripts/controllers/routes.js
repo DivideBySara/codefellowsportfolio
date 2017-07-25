24917d0ee56c7f0e1f387bd13b6305f12d9cf0d6
Sara Jade <wisconsinsara@gmail.com>
2017-07-25 11:43:01 -0700

    Update route.js functions

--- public/scripts/controllers/routes.js
+++ public/scripts/controllers/routes.js
@@ -1,10 +1,11 @@
+/* jshint browser: true, devel: true, esversion: 6 */
 'use strict';
 
 var application = application || {};
 
 // route for index.html
-page('/', application.websiteController.initWebsiteController());
+page('/', application.websiteController.getWebsitesSection());
 // route for about.html
-page('/about', application.aboutController.initAboutController());
+page('/about', application.aboutController.getAboutSection());
 // call page()
 page();
