9a662a0880fabb87de74f66a8b55345c5053a650
Sara Jade <wisconsinsara@gmail.com>
2017-07-27 13:43:35 -0700

    Update renderedRepos to renderRepos for clarity in githubReposView.js

--- public/scripts/views/githubReposView.js
+++ public/scripts/views/githubReposView.js
@@ -9,8 +9,8 @@ var application = application || {};
   // A global view object holds the functions
   const githubReposView = {};
 
-  // Handlebars generates repos
-  const renderedRepos = Handlebars.compile($('#repoTemplate').html());
+  // Handlebars generates repos. renderRepos is a function!
+  const renderRepos = Handlebars.compile($('#repoTemplate').html());
 
   // Next line provides application.githubReposView = githubReposView
   module.githubReposView = githubReposView;
@@ -19,7 +19,7 @@ var application = application || {};
     // Removes <li>'s to prepare for reloading them
     $('#githubRepos').children('ul').empty();
     // Map repos
-    let mappedRepos = application.githubRepos.all.map(renderedRepos)
+    let mappedRepos = application.githubRepos.all.map(renderRepos);
     // Append repos
     $('#githubRepos ul').append(mappedRepos);
   };
